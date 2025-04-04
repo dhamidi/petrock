df0d1db19c7309fab7f34bb58a9e3e4160f8e912
Dario Hamidi <dario.hamidi@gmail.com>
2025-04-04 11:41:55 +0300

    chore: remove unused file

--- internal/skeletonfs/skeletonfs.go
+++
@@ -1,9 +0,0 @@
-package skeletonfs
-
-import "embed"
-
-// SkeletonFS holds the embedded filesystem for the project skeleton.
-// The path is relative from this file's directory (internal/skeletonfs)
-// to the target directory (internal/skeleton).
-//go:embed all:../skeleton
-var SkeletonFS embed.FS
